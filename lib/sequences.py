163968c91a82836d03d7022f68551709ae193c76
Travis <travis.williams94@gmail.com>
2023-07-03 10:31:48 -0600

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    seq = []
+    if length > 0:
+        seq.append(0)
+        if length > 1:
+            seq.append(1)
+            if length > 2:
+                for i in range(2, length):
+                    seq.append(
+                        seq[i-1] + seq[i-2]
+                    )
+    print(seq)
